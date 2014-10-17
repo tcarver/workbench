3c78cd89500b570336e9b75c2622094c3f641db1
tcarver <tjc29@aspen.dil.private.cimr.cam.ac.uk>
2014-10-17 09:29:56 +0100

    add WorkBenchTree.pm

--- lib/WorkBench.pm
+++ lib/WorkBench.pm
@@ -1,10 +1,7 @@
 package WorkBench;
 use Dancer ':syntax';
-
-use GDxBase::Persistable::Folder;
-use GDxBase::DBHandler;
+use WorkBenchTree;
 use Data::Dumper;
-
 
 get '/workbench/' => sub {
 	my $ini_file = config->{CONFIG}; # config 
@@ -14,7 +11,7 @@ get '/workbench/' => sub {
 
 	my $where = "visibility='public'";
 	my @objs  = GDxBase::Persistable::Folder->fetch_all( where => $where );
-	my %tree  = treeObject2Hash(\@objs);
+	my %tree  = WorkBenchTree->treeObject2Hash(\@objs);
 
 	template 'workbench', {
 		'tree' => \%tree,
@@ -27,79 +24,5 @@ post '/workbench/' => sub {
 	return GDxBase::Persistable->get_preferred_name(
 	         params->{folderId}, params->{perlClass});
 };
-
-sub treeObject2Hash {
-	my @objs = @{$_[0]};
-	my %tree = ();
-	my @childTree;
-	foreach my $node ( @objs ) {
-		if($node && $node->isa('GDxBase::Persistable::Folder')) {
-			my $name = $node->name;
-			my $parent = $node->folder_id;
-			my $passed = $name;
-            $passed =~ s/\s/_/g;
-
-			my @node_details;
-			my $classoids = $node->oids;
-          	foreach my $class (keys %{$classoids}) {
-    	    	next unless scalar $classoids->{$class};
-            	foreach my $oid (@{ $classoids->{$class} }) {
-              		next unless $oid;
-
-	      			if($class eq 'GDxBase::Persistable::Folder') {
-	      				#nested folder
-                		my %sub_tree = getChildNodeTree(\@objs, $oid);
-                		#print Dumper(%sub_tree);
-                		my $perlClass = $class;
-                		$perlClass =~ s/::/__/g;
-              		   	push(@node_details, 
-              		   		{ 'oid'   => $oid, 'class' => $perlClass, 
-              		   		  'passed'=> $passed, 'parent_id'=>$parent, 'child' => \%sub_tree });
-              		   	push(@childTree, keys %sub_tree);
-              		} else {
-              			my $perlClass = $class;
-              		  	$perlClass =~ s/::/__/g;
-              		   	push(@node_details, {'oid'=>$oid, 'class'=>$perlClass, 'passed'=> $passed, 'parent_id'=>$parent });
-              		}
-            	}
-          	}
-          	$tree{$name} = [ @node_details ];
-		}
-    }
-
-    delete @tree { @childTree }; # remove child folder nodes from top level
-    return %tree;
-}
-
-sub getChildNodeTree {
-	my @objs = @{$_[0]};
-	my $child_folder_id = $_[1];
-	foreach my $node ( @objs ) {
-		if($node && $node->isa('GDxBase::Persistable::Folder')) {
-			my $folder_id = $node->folder_id;
-			if($folder_id == $child_folder_id) {
-				my %tree = ();
-				my $name = $node->name;
-				my $passed = $name;
-            	$passed =~ s/\s/_/g;
-
-				my @node_details;
-				my $classoids = $node->oids;
-          		foreach my $class (keys %{$classoids}) {
-    	    		next unless scalar $classoids->{$class};
-            		foreach my $oid (@{ $classoids->{$class} }) {
-              			next unless $oid;
-	      				my $perlClass = $class;
-              		  	$perlClass =~ s/::/__/g;
-              		   	push(@node_details, {'oid'=>$oid, 'class'=>$perlClass, 'passed'=> $passed });
-            		}
-          		}
-          		$tree{$name} = [ @node_details ];
-				return %tree;
-			}
-		}
-    }
-    return -1;
-}
 
 true;
